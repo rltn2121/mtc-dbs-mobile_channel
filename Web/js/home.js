a4506ae304e5166b9140c2aac1f0415a14c0ef1d
22201718 <bacccine@shinhan.com>
2023-11-11 16:27:19 +0900

    home 화면 작업 임시 저장

--- Web/js/home.js
+++ Web/js/home.js
@@ -3,5 +3,13 @@
 */
 
 /*
-버튼이벤트
+페이지 이동함수
 */
+function goNextPage(page) {
+  var page = page;
+
+  if (page == "payment") {
+  } else if (page == "transaction") {
+  } else if (page == "charge") {
+  }
+}
