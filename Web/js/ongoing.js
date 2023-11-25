729b034473f312e75e4e8bee798a9d8ec3c6569f
22201718 <bacccine@shinhan.com>
2023-11-25 15:46:17 +0900

    ongoing 수정

--- Web/js/ongoing.js
+++ Web/js/ongoing.js
@@ -12,21 +12,30 @@ function drawLoading() {
 }
 /*
 결제모듈 호출
+거래결과
+(0: 진행 중, 1: 결제성공, 2: 충전성공, 
+3: 결제실패, 4: 충전실패, 99:원장정보없음)
 */
 function callPost() {
   var ascer = isNull(sessionStorage.getItem("exgAcser"))
     ? sessionStorage.getItem("payAcser")
     : sessionStorage.getItem("exgAcser");
-  fetch(url + "/wallet/isSuccess/" + ascer)
+  console.log(ascer);
+  fetch(url + "/wallet/chk/" + ascer)
     .then((response) => response.json())
     .then((data) => {
       var data = data;
-
-      if (!isNull(data.err_msg)) {
-        showError("결제 실행 중 실패했습니다.\n" + data.errStr);
-      } else {
+      console.log(data);
+      if (data.upmuG <= 2 && data.upmuG >= 1) {
         //성공시
         goNextPage("complete");
+      } else if (!isNull(data.err_msg) || data.upmuG >= 3) {
+        showError(
+          "결제 실행 중 실패했습니다.\n\nErrCode " +
+            data.upmuG +
+            ": " +
+            data.err_msg
+        );
       }
     });
 }
